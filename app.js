d2dad1cdf39943b7e4ec2f7bbb8fd678139b1ae2
ADARIYA0 <adrianianimportant@gmail.com>
2025-09-26 07:53:37 +0700

    REMOVE: Menghilangkan fungsi siswa

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const cors = require('cors');
 const corsOptions = require('./src/config/corsOption');
 const express = require('express');
 const helmet = require('helmet');
-const multer = require('multer');
-const path = require('path');
-const siswaRoutes = require('./src/routes/siswaRoute');
 const umumRoutes = require('./src/routes/umumRoute');
 
 const app = express();
@@ -19,29 +16,6 @@ app.use(helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" }
 }));
 
-app.use('/api/siswa', siswaRoutes);
 app.use('/api/umum', umumRoutes);
-
-function imageOnly(req, res, next) {
-    const ext = path.extname(req.path).toLowerCase();
-    if (!['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
-        return res.status(403).json({ error: 'Akses file tidak diizinkan' });
-    }
-    next();
-}
-
-app.use('/uploads/siswa', imageOnly, express.static(path.join(__dirname, 'uploads/siswa')));
-
-app.use((err, req, res, next) => {
-    if (err instanceof multer.MulterError) {
-        if (err.code === 'LIMIT_FILE_SIZE') {
-            return res.status(400).json({ error: 'Ukuran file terlalu besar. Maksimal 5MB.' });
-        }
-        return res.status(400).json({ error: err.message });
-    } else if (err) {
-        return res.status(500).json({ error: 'Terjadi kesalahan saat upload file.' });
-    }
-    next();
-});
 
 module.exports = app;
